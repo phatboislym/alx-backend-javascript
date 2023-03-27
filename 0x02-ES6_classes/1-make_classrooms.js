4692964ed782608558920f0af0cdfba1572187c1
Adewole Conde <phatboislym@protonmail.com>
2023-03-27 21:25:05 +0100

    add: task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom.js';
+
+function initializeRooms() {
+  const x1 = new ClassRoom(19);
+  const x2 = new ClassRoom(20);
+  const x3 = new ClassRoom(34);
+  const x4 = [x1, x2, x3];
+  return (x4);
+}
+module.exports = initializeRooms;
