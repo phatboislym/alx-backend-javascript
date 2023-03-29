d17901ecac835a809c6a8c2272e105912aa148af
Adewole Conde <phatboislym@protonmail.com>
2023-03-29 23:12:54 +0100

    add: task 1

---
+++ 0x04-Typescript/task_1/js/main.ts
@@ -0,0 +1,29 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [prop: string]: any,
+}
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string) {
+    return `${firstName.charAt(0)}. ${lastName}`
+}
+
+class StudentClass {
+    firstName: string;
+    lastName: string;
+    
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+}
+workOnHomework = () : string => 'Currently working'
+
+displayName = () : string => this.firstName
+}
