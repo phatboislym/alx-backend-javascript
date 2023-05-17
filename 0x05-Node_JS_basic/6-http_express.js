05794098c1be57eacb9b84b2a5780f69824cf315
Adewole Conde <phatboislym@protonmail.com>
2023-05-17 23:31:39 +0100

    add: task 6

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+app.get('/', (request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
