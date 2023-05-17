5ef088328079c631b49f0ba7624d952fdb9f8b78
Adewole Conde <phatboislym@protonmail.com>
2023-05-17 10:41:10 +0100

    fix: lint fixes

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,12 +4,12 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((request, response) => {
-	  response.statusCode = 200;
-	  response.setHeader('Content-Type', 'text/plain');
-	  response.end('Hello Holberton School!');
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
-	  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 module.exports = app;
