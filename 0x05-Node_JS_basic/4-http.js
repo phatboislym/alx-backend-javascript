f9f14afd7a1b64db207e94edf57f204730cef3c1
Adewole Conde <phatboislym@protonmail.com>
2023-05-17 00:00:29 +0000

    revert changes

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,12 +3,13 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((req, res) => {
-	  res.statusCode = 200;
-	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello Holberton School!');
+const app = http.createServer((request, response) => {
+	  response.statusCode = 200;
+	  response.setHeader('Content-Type', 'text/plain');
+	  response.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
 	  console.log(`Server running at http://${hostname}:${port}/`);
 });
+module.exports = app;
