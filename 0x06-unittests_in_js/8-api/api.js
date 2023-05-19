a3b8c3164f893aed6b69b41b4e1aa2c826137cd5
Adewole Conde <phatboislym@protonmail.com>
2023-05-20 00:18:28 +0100

    fixes

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -12,5 +12,3 @@ app.get('/', (request, response) => {
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
 });
-
-module.exports = app;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,26 +1,14 @@
+const request = require('request');
 const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('./api');
 
-chai.use(chaiHttp);
 const { expect } = chai;
 
-describe('index Page', () => {
-  it('should return status code 200', () => new Promise((done) => {
-    chai.request(app)
-      .get('/')
-      .end((error, response) => {
-        expect(response).to.have.status(200);
-        done();
-      });
-  }));
-
-  it('should return the correct result', () => new Promise((done) => {
-    chai.request(app)
-      .get('/')
-      .end((error, response) => {
-        expect(response.text).to.equal('Welcome to the payment system');
-        done();
-      });
-  }));
+describe('call the index route /', () => {
+  it('GET /', () => function (done) {
+    request('http://localhost:7865/', (error, response, body) => {
+      expect(response).to.have.status(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
 });
