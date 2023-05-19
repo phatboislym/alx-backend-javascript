fb90a281a1e34a86787ae471e2607979b113138c
Adewole Conde <phatboislym@protonmail.com>
2023-05-19 01:32:45 +0100

    fix: adjust errant closing parentheses

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -5,12 +5,12 @@ const expect = chai.expect;
 
 describe('calculateNumber', () => {
   it('performs operation of type param on rounded number params', () => {
-    expect(calculateNumber('SUM', 3.2, 4.6).to.deep.equal(8));
+    expect(calculateNumber('SUM', 3.2, 4.6)).to.deep.equal(8);
 
-    expect(calculateNumber('SUBTRACT', -2.8, 1.4).to.deep.equal(-4));
+    expect(calculateNumber('SUBTRACT', -2.8, 1.4)).to.deep.equal(-4);
 
-    expect(calculateNumber('DIVIDE', 50, 25).to.deep.equal(2));
+    expect(calculateNumber('DIVIDE', 50, 25)).to.deep.equal(2);
 
-    expect(calculateNumber('DIVIDE', 5, 0.5).to.deep.equal('Error'));
+    expect(calculateNumber('DIVIDE', 5, 0.5)).to.deep.equal('Error');
   });
 });
