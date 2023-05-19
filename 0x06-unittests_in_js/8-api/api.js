059cbed231b42951c2e41bca36453b9011553c6e
Adewole Conde <phatboislym@protonmail.com>
2023-05-19 14:29:31 +0100

    add: task 8

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.get('/', (request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.send('Welcome to the payment system');
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
