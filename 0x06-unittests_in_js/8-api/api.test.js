b910517311da83cdb45576a65a1b1ffe58c0e477
Adewole Conde <phatboislym@protonmail.com>
2023-05-21 10:57:31 +0100

    more fixes

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,11 +1,11 @@
 const request = require('request');
 const chai = require('chai');
 
-const { expect } = chai;
+const expect = chai.expect;
 
-describe('index page /', () => {
-  it('GET /', () => function (done) {
-    request('http://localhost:7865/', (error, response, body) => {
+describe('index page', () => {
+  it('tests the / endpoint', function (done) {
+    request('http://localhost:7865', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
       done();
